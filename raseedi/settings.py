2650fc883e2c642be4c58d4f137589362edce75a
thomas <thomas.adel31@gmail.com>
2019-10-21 10:03:13 +0200

    fix jwt

--- raseedi/settings.py
+++ raseedi/settings.py
@@ -81,7 +81,7 @@ DATABASES = {
 
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': (
-        # 'rest_framework_jwt.authentication.JSONWebTokenAuthentication',
+        'rest_framework_jwt.authentication.JSONWebTokenAuthentication',
     ),
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
     'PAGE_SIZE': 10
